6426f0167f521c9e637a0adfd588a8731a9c2211
fanwise <fanwise.901@gmail.com>
2015-11-25 23:51:33 +0800

    game.js:Add acceleration test.

--- game.js
+++ game.js
@@ -7,25 +7,56 @@ window.onload = function(){
 
           cc.log("HelloWorld init")
 
+
+
           return true;
         },
         onEnter:function () {
           this._super();
                               
           var size = cc.winSize;
-          var rect = new cc.DrawNode();
-          rect.drawRect(cc.p(0,0),cc.p(300,300),cc.color(),5,cc.color(255,0,0,255));
-          this.addChild(rect);
-          rect.setPositionX(size.width/2);
-          rect.setPositionY(size.height/2);
 
-          rect.setContentSize(cc.size(300,300));
-          rect.setAnchorPoint(cc.p(0.5,0.5));
+          var SPEED = 10;
 
+          var bg = new cc.Sprite(res.Background);  
+          bg.x = size.width/2;  
+          bg.y = size.height/2;  
+          this.addChild(bg, 0, 0);  
 
-          this.schedule(function(f){
-            rect.setRotation(rect.getRotation()+1);
-          })
+          var ball = new cc.Sprite(res.Ball);
+          ball.x = size.width/2;
+          ball.y = size.height/2;
+          this.addChild(ball,10);
+
+          cc.inputManager.setAccelerometerEnabled(true);
+          
+            cc.eventManager.addListener({
+                event: cc.EventListener.ACCELERATION, 
+                callback: function(acc, event){ 
+                    var size = cc.director.getWinSize();  
+                    var s = ball.getContentSize();
+                    var p0 = ball.getPosition();  
+  
+  
+                    var p1x =  p0.x + acc.x * SPEED ;  
+                    if ((p1x - s.width/2) <0) {  
+                        p1x = s.width/2;
+                    }  
+                    if ((p1x + s.width / 2) > size.width) {
+                        p1x = size.width - s.width / 2; 
+                    }  
+  
+  
+                    var p1y =  p0.y + acc.y * SPEED ;   
+                    if ((p1y - s.height/2) < 0) {  
+                        p1y = s.height/2;  
+                    }  
+                    if ((p1y + s.height/2) > size.height) {  
+                        p1y = size.height - s.height/2;  
+                    }  
+                    ball.runAction(cc.place(cc.p( p1x, p1y))); 
+                }  
+            }, ball);  
         }
       });
       cc.director.runScene(new MyScene());
---
+++ resource.js
@@ -0,0 +1,9 @@
+var res = {
+	Ball : "res/ball.png",
+	Background : "res/background.png"
+};
+
+var g_resources = [];
+for(var i in res) {
+	g_resources.push(res[i]);
+}
